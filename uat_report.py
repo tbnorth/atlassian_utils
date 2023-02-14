ea38acb82ebcaf87ab333be78a39e60e9d140fff
Terry Brown <Brown.TerryN@epa.gov>
2023-02-14 13:12:27 -0500

    function replacing repetition

--- uat_report.py
+++ uat_report.py
@@ -15,13 +15,18 @@ def summary_markdown(issue):
     )
 
 
+def header(issue, level):
+    """Markdown header for an issue."""
+    return f"\n{'#'*level} {issue['key']} - {issue['fields']['summary']}\n"
+
+
 query = f"issueKey = {ENV['ATL_ISSUE_KEY']}"
 epic = atl_util.jql_result(jira, query, use_default=True)[0]
-print(f"# {epic['key']} - {epic['fields']['summary']}")
+print(header(epic, 1))
 print(summary_markdown(epic))
 for issue in jira.epic_issues(epic["key"])["issues"]:
-    print(f"## {issue['key']} - {issue['fields']['summary']}")
+    print(header(issue, 2))
     print(summary_markdown(issue))
     for sub in jira.jql_get_list_of_tickets(f"parent = {issue['key']}"):
-        print(f"### {sub['key']} - {sub['fields']['summary']}")
+        print(header(sub, 3))
         print(summary_markdown(sub))
