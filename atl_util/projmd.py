81d1fb08ef997cd7e11bd8ece6a68c776fd7ea8c
Terry Brown <Brown.TerryN@epa.gov>
2023-08-14 11:12:25 -0400

    incl. budget calc.
    to avoid missing updates

--- atl_util/projmd.py
+++ atl_util/projmd.py
@@ -7,6 +7,7 @@ import json
 from sys import stderr
 
 import atl_util
+from atl_util import ENV
 
 
 def issue_deliv_url(data):
@@ -27,6 +28,8 @@ sorting = [i for i in sorting if i[1].get("in_budget", True)]
 sorting.sort(key=lambda x: x[1].get("timing", "Q0"))
 text = []
 text.append(f"# Project Plan {data['generated']}\n")
+sep = ["  \n---\n  \n"]
+text += sep
 
 # Long form list of deliverables
 for epic, deliv, url in sorting:
@@ -35,6 +38,7 @@ for epic, deliv, url in sorting:
         f"  {deliv['purpose']}  ",
         f"  {deliv['history']}  ",
     ]
+text += sep
 
 # Cost estimates table
 text += ["", "|Epic|API (weeks)|UI (weeks)|", "|---|---|---|"]
@@ -52,6 +56,7 @@ for epic, deliv, url in sorting:
     row.append("")
     text.append("|".join(row))
 text.append(f"||{beans['api']['total']:.1f}|{beans['ui']['total']:.1f}|")
+text += sep
 
 # Timeline table
 text += ["", "|Item|Delivery|", "|---|---|"]
@@ -62,5 +67,39 @@ for epic, deliv, url in sorting:
     text.append(
         "|".join(["", f"[{epic['key']}]({url}) {epic['summary']}", timeline, ""])
     )
+text += sep
+
+# Cost calcs.
+rate = int(ENV["ATL_CONTRACTOR_RATE"])
+# api 0 ui 100 -> {"api": 0, "ui": 100}
+prop = iter(ENV["ATL_CONTRACTOR_PROP"].split(" "))
+prop = dict((i, float(next(prop))) for i in prop)
+text += [f"Using the suggested ${rate}/hr for contractor budgeting:", ""]
+for team in "api", "ui":
+    name = team.upper()
+    text += [f"\n{name}", ""]
+    beans[team]["hours"] = int(beans[team]["total"] * 40 * prop[team] / 100)
+    beans[team]["dollars"] = int(beans[team]["hours"] * rate)
+    text += [
+        f"{beans[team]['total']} \"100%\" "
+        f"contractor weeks = {beans[team]['hours']} hours",
+        "",
+        f"{beans[team]['hours']} x ${rate:,} = ${beans[team]['dollars']:,}",
+        "",
+        "But this would be reduced proportional "
+        f"to the level of Fed. {name} involvement.",
+    ]
+contract = beans["api"]["dollars"] + beans["ui"]["dollars"]
+maint = int(contract * 0.2)
+text += [
+    "", "Total cost:", "",
+    f"- Contractor cost: ${contract:.0f}",
+    f"- Maintenance of existing code, updates, bug fixes etc. at 20%: ${maint:,}",
+    f"- Total: ${contract + maint:,}",
+    "- Fed. 40 hour weeks",
+    f"  - API: 0-{beans['api']['total']} depending on contractor use",
+    f"  - UI: 0-{beans['ui']['total']} depending on contractor use",
+]
+
 
 print("\n".join(text))
